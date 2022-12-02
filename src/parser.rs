d62f2b758665c48b9d2b28b098960622c563e44e
Niland Schumacher <naschuma@ucsc.edu>
2022-12-01 17:47:55 -0800

    moving work

--- src/parser.rs
+++ src/parser.rs
@@ -14,6 +14,15 @@ struct State<'a> {
   items: &'a [Item<'a>]
 }
 
+struct Parser<'a> {
+  states: &'a [State<'a>],
+  // propagation table
+  // stack
+  // action table
+  // move table
+  // AST
+}
+
 impl<'a> Item<'a> {
   fn new(head: NT, symbol_vec: Vec<Symbol>, la: Symbol) -> &'a Item<'a> {
     let body: &[Symbol] = &symbol_vec;
@@ -90,10 +99,66 @@ impl<'a> Item<'a> {
   }
 }
 
-impl<'a> State<'a> {
-  fn goto(self, symbol: Symbol) {
-    todo!();
+fn slr_move(state: State, move_symbol: Symbol) -> State {
+  let new_state_items = vec![];
+  let mut closure_items: Vec<Item> = Vec::new();
+  slr_closure(state, &mut closure_items);
+
+  for item in closure_items {
+    if item.expects() == move_symbol {
+      let new_item = Item::new(item.head, item.shift_cursor(), item.la);
+      new_state_items.push(new_item);
+    }
   }
 
-  // two different kinds of closures
+  //let new_state = State {
+  //  id: newStateId(),
+  //  items: &new_state_items
+  //}
+
+  // create a new state
+  // break if new state exists
+  // add state
+  State{id:0, items: Vec::new()}
+}
+
+fn slr_closure(state: State, result: &mut Vec<Item>) {
+  for old_item in state.items.iter() {
+    let symbol_vec = old_item.shift_cursor();
+    let new_item = Item::new(old_item.head, symbol_vec, old_item.la);
+    result.push(*new_item);
+  }
+}
+
+impl<'a> Parser<'a> {
+  pub fn new() {
+
+  }
+
+  fn init_state() {
+
+  }
+
+  fn slr_move<'func>(self, state: State) -> State {
+    let mut closure_items: Vec<Item> = Vec::new();
+    self.slr_closure(state, &mut closure_items);
+
+    state
+  }
+
+  fn slr_closure(self, state: State, result: &mut Vec<Item>) {
+    for old_item in state.items.iter() {
+      let symbol_vec = old_item.shift_cursor();
+      let new_item = Item::new(old_item.head, symbol_vec, old_item.la);
+      result.push(*new_item);
+    }
+  }
+
+  fn lalr_move() {
+
+  }
+
+  fn lalr_closure() {
+
+  }
 }
