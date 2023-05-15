025764e4f79a3f3114f1f0e4f20568badc8d6cf8
Niland Schumacher <naschuma@ucsc.edu>
2023-05-14 18:45:33 -0700

    config args bug

--- src/config.rs
+++ src/config.rs
@@ -4,12 +4,16 @@ pub struct Config<'a> {
 
 impl<'a> Config<'a> {
     pub fn build(args: &'a Vec<String>) -> Result<Config, &'static str> {
-        if args.len() != 1 {
+        if args.len() > 2 {
             return Err("Too many args");
         }
 
+        if args.len() == 1 {
+            return Err("No filename passed");
+        }
+
         Ok(Self {
-            filename: args[0].as_str(),
+            filename: args[1].as_str(),
         })
     }
 }
